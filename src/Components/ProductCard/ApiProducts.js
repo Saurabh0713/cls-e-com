9c8b0887301cc9136d711fae9fd8edeae96d81f3
Saurabh0713 <109545639+Saurabh0713@users.noreply.github.com>
2023-06-03 00:07:54 +0530

    api

--- src/Components/ProductCard/ApiProducts.js
+++ src/Components/ProductCard/ApiProducts.js
@@ -12,7 +12,7 @@ export default function ApiProducts() {
    },[])
 
   return (
-    <div className='container d-flex flex-wrap mt-5'>
+    <div className='container d-flex flex-wrap mt-5 min-vh-100'>
           {product.map((value,index)=>(
         <div key={index} className='rounded m-3'>
         <ApiProductCard title={value.title} price={value.price} image={value.image} description={value.description} rating={value.rating.rate} ratingCount={value.rating.count}/>
